b2efd228bed9cdd9678c5c6ad0fb28afee6f1c94
Aleksandr Gordeev <razamanaza@yandex.ru>
2019-11-11 12:51:19 +1300

    Finish main screen

---
+++ app/src/main/java/gorda5/bti/famouspaintingsquest/PaintersQuiz.java
@@ -0,0 +1,14 @@
+package gorda5.bti.famouspaintingsquest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaintersQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_painters_quiz);
+    }
+}
